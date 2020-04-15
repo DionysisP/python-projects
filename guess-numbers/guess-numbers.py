b7aa03913bb1f95c88bd2f1b0dd399a3bc98de46
Dionysis Patsiouras <58115790+DionysisP@users.noreply.github.com>
2020-04-15 04:05:21 +0300

    Add files via upload

--- guess-numbers/guess-numbers.py
+++ guess-numbers/guess-numbers.py
@@ -1,33 +1,54 @@
-
 import random
 
-kino_numbers = []
-winning_numbers = []
+
 numbers = []
 
-#fill kino numbers list with numbers
-for i1 in range(20):
-    i1 += 1
-    kino_numbers.append(i1)
+attemps = 1
+isRunning = True
 
-#declare lucky numbers
-for i2 in range(5):
-    choice = random.choice(kino_numbers)
-    kino_numbers.remove(choice)
-    winning_numbers.append(choice)
+while isRunning:
 
-#input numbers
-for i3 in range(5):
-    x = input("Give me 5 numbers, 1 - 20\n")            
-    numbers.append(x)
+    kino_numbers = []
+    kino_numbers2 = []
+    winning_numbers = []
+    ainums = []
 
-print("Your numbers:", *numbers)
-print("The wining numbers are:", *winning_numbers)
+    
+    #fill kino numbers list with numbers
+    for i1 in range(20):
+        i1 += 1
+        kino_numbers.append(i1)
+        kino_numbers2.append(i1)
 
 
-if numbers[1:5] == winning_numbers[1:5]:
-    print("YOU WIN")
-else:
-    print("You lost")
+    #declare lucky numbers
+    for i2 in range(5):
+        choice = random.choice(kino_numbers)
+        winning_numbers.append(choice)
+        kino_numbers.remove(choice)
+        winning_numbers.sort()
+
+
+
+#AI number choices
+    for i4 in range(5):
+        x2 = random.choice(kino_numbers2)
+        ainums.append(x2)
+        kino_numbers2.remove(x2)
+        ainums.sort()
+
+    print("AI numbers:", *ainums)
+    print("The wining numbers are:", *winning_numbers)
+
+
+
+    if ainums[0:5] == winning_numbers[0:5]:
+        print("Ai finally found the numbers")
+        print("Number of attempts:")
+        print(attemps)
+        isRunning = False
+    else:
+        print("AI didnt win\n")
+        attemps += 1
 
 
